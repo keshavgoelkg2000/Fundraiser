2f13daa9e6dd53c7eda18b08e941bf011b1b4a7d
Keshav Goel <keshavgoelkg2000@gmail.com>
2023-10-31 19:25:56 +0530

    fundraiser

---
+++ contract/contracts/Contract.sol
@@ -0,0 +1,243 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Fundraiser {
+    struct CampaignData {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+    }
+
+    struct Campaign {
+        CampaignData data;
+        mapping(address => uint256) donatedAmounts; // Track individual donation amounts
+        address[] donators;
+        uint256[] donations; // Track donation amounts
+        mapping(address => RefundRequest) refundRequests; // Track refund requests
+    }
+
+    struct RefundRequest {
+        address requester;
+        uint256 amount;
+        bool processed;
+    }
+
+    mapping(uint256 => Campaign) public campaigns;
+    uint256 public numberOfCampaigns = 0;
+
+    function createCampaign(
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline,
+        string memory _image
+    ) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        require(
+            _deadline > block.timestamp,
+            "The deadline should be a date in the future."
+        );
+
+        campaign.data.owner = _owner;
+        campaign.data.title = _title;
+        campaign.data.description = _description;
+        campaign.data.target = _target;
+        campaign.data.deadline = _deadline;
+        campaign.data.amountCollected = 0;
+        campaign.data.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+    }
+
+    function donateToCampaign(uint256 _id) public payable {
+        require(msg.value > 0, "Donation amount should be greater than 0.");
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+        campaign.donatedAmounts[msg.sender] += amount;
+
+        (bool sent, ) = payable(campaign.data.owner).call{value: amount}("");
+
+        if (sent) {
+            campaign.data.amountCollected += amount;
+        }
+    }
+
+    function requestRefund(uint256 _id) public payable {
+        Campaign storage campaign = campaigns[_id];
+        require(campaign.data.amountCollected > 0, "No donations to refund.");
+
+        uint256 donationAmount = campaign.donatedAmounts[msg.sender];
+        require(donationAmount > 0, "No donation from the sender.");
+
+        if (campaign.refundRequests[msg.sender].processed == true) {
+            campaign.refundRequests[msg.sender].processed = false;
+        } else {
+            campaign.refundRequests[msg.sender] = RefundRequest({
+                requester: msg.sender,
+                amount: donationAmount,
+                processed: false
+            });
+        }
+    }
+
+    function processRefund(uint256 _id, address payable _donor) public payable {
+        Campaign storage campaign = campaigns[_id];
+        require(
+            campaign.data.owner == msg.sender,
+            "Only the campaign owner can process refunds."
+        );
+        require(
+            campaign.refundRequests[_donor].processed == false,
+            "Refund not requested by the donor."
+        );
+
+        uint256 donationAmount = campaign.donatedAmounts[_donor];
+        require(donationAmount > 0, "No donation from the donor.");
+
+        campaign.data.amountCollected -= donationAmount;
+        campaign.donatedAmounts[_donor] = 0;
+        campaign.refundRequests[_donor].processed = true;
+
+        (bool sent, ) = _donor.call{value: donationAmount}("");
+        require(sent, "Failed to send refund.");
+
+        // Remove the donor from the donators array
+        uint256 donorIndex;
+        for (uint256 i = 0; i < campaign.donators.length; i++) {
+            if (campaign.donators[i] == _donor) {
+                donorIndex = i;
+                break;
+            }
+        }
+        if (donorIndex < campaign.donators.length - 1) {
+            // Move the last element to the position of the removed donor
+            campaign.donators[donorIndex] = campaign.donators[
+                campaign.donators.length - 1
+            ];
+            campaign.donations[donorIndex] = campaign.donations[
+                campaign.donators.length - 1
+            ];
+        }
+        // Remove the last element from the array
+        campaign.donators.pop();
+        campaign.donations.pop();
+    }
+
+    function deleteCampaign(uint256 _id) public {
+    require(_id < numberOfCampaigns, "Invalid campaign ID.");
+
+    Campaign storage campaignToDelete = campaigns[_id];
+    require(campaignToDelete.data.owner == msg.sender, "Only the campaign owner can delete the campaign.");
+
+    // Refund donations and process refund requests
+    address[] storage donators = campaignToDelete.donators;
+    for (uint256 i = 0; i < donators.length; i++) {
+        address donator = donators[i];
+        uint256 donationAmount = campaignToDelete.donatedAmounts[donator];
+        if (donationAmount > 0) {
+            campaignToDelete.donatedAmounts[donator] = 0;
+            (bool sent, ) = payable(donator).call{value: donationAmount}("");
+            require(sent, "Failed to refund donation.");
+        }
+        campaignToDelete.refundRequests[donator].processed = true;
+    }
+
+    // Swap the campaign to delete with the last campaign
+    uint256 lastCampaignId = numberOfCampaigns - 1;
+    Campaign storage lastCampaign = campaigns[lastCampaignId];
+
+    // Copy data from the last campaign to the campaign to delete
+    campaignToDelete.data.owner = lastCampaign.data.owner;
+    campaignToDelete.data.title = lastCampaign.data.title;
+    campaignToDelete.data.description = lastCampaign.data.description;
+    campaignToDelete.data.target = lastCampaign.data.target;
+    campaignToDelete.data.deadline = lastCampaign.data.deadline;
+    campaignToDelete.data.amountCollected = lastCampaign.data.amountCollected;
+    campaignToDelete.data.image = lastCampaign.data.image;
+
+    // Update the mappings of the moved campaign's donators
+    for (uint256 i = 0; i < campaignToDelete.donators.length; i++) {
+        address donator = campaignToDelete.donators[i];
+        campaignToDelete.donatedAmounts[donator] = lastCampaign.donatedAmounts[donator];
+        campaignToDelete.refundRequests[donator] = lastCampaign.refundRequests[donator];
+    }
+
+    // Delete the last campaign from the mapping
+    delete campaigns[lastCampaignId];
+
+    // Decrement the numberOfCampaigns count
+    numberOfCampaigns--;
+}
+
+
+
+
+    function getDonators(
+        uint256 _id
+    ) public view returns (address[] memory, uint256[] memory) {
+        Campaign storage campaign = campaigns[_id];
+        return (campaign.donators, campaign.donations);
+    }
+
+    function getCampaigns() public view returns (CampaignData[] memory) {
+        CampaignData[] memory allCampaigns = new CampaignData[](
+            numberOfCampaigns
+        );
+
+        for (uint256 i = 0; i < numberOfCampaigns; i++) {
+            allCampaigns[i] = campaigns[i].data;
+        }
+
+        return allCampaigns;
+    }
+
+    function getRefundRequests(
+        uint256 _id
+    ) public view returns (RefundRequest[] memory) {
+        Campaign storage campaign = campaigns[_id];
+        RefundRequest[] memory requests = new RefundRequest[](
+            campaign.donators.length
+        );
+
+        for (uint256 i = 0; i < campaign.donators.length; i++) {
+            address donator = campaign.donators[i];
+            requests[i] = campaign.refundRequests[donator];
+        }
+
+        return requests;
+    }
+
+    function getDonorCampaigns(
+        address _donor
+    ) public view returns (CampaignData[] memory) {
+        uint256 count = 0;
+        for (uint256 i = 0; i < numberOfCampaigns; i++) {
+            if (campaigns[i].donatedAmounts[_donor] > 0) {
+                count++;
+            }
+        }
+
+        CampaignData[] memory donorCampaigns = new CampaignData[](count);
+        uint256 currentIndex = 0;
+        for (uint256 i = 0; i < numberOfCampaigns; i++) {
+            if (campaigns[i].donatedAmounts[_donor] > 0) {
+                donorCampaigns[currentIndex] = campaigns[i].data;
+                currentIndex++;
+            }
+        }
+
+        return donorCampaigns;
+    }
+}
